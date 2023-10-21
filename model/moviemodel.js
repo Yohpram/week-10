4bba95beecf4da7fc55ae1e28f3b2269ca66564e
Yohpram <126755927+Yohpram@users.noreply.github.com>
2023-10-21 15:17:25 +0700

    Update moviemodel.js

--- model/moviemodel.js
+++ model/moviemodel.js
@@ -1,48 +1,59 @@
 const pool = require ("../config/connection");
 
-
 class MoviesModel {
-    static async getAllMovies() {
-      const query = 'SELECT * FROM public.movies;';
-      try {
-        const { rows } = await pool.query(query);
-        return rows;
-      } catch (error) {
-        throw error;
-      }
-    }
+  constructor(id, title, genres, year, photos) {
+    this.id = +id;
+    this.title = title;
+    this.genres = genres;
+    this.year = year;
+    this.photos = photos;
+  }
 
-    static async createMovie(movie) {
-        const { title, genres, year, photos } = movie;
-        const query = 'INSERT INTO public.movies (title, genres, year, photos) VALUES ($1, $2, $3, $4) RETURNING id;';
-        try {
-          const { rows } = await pool.query(query, [title, genres, year, photos]);
-          return rows[0].id;
-        } catch (error) {
-          throw error;
-        }
-      }
-    
-      static async updateMovie(id, updatedMovie) {
-        const { title, genres, year, photos } = updatedMovie;
-        const query = 'UPDATE public.movies SET title = $1, genres = $2, year = $3, photos = $4 WHERE id = $5;';
-        try {
-          await pool.query(query, [title, genres, year, photos, id]);
-          return true;
-        } catch (error) {
-          throw error;
-        }
-      }
-    
-      static async deleteMovie(id) {
-        const query = 'DELETE FROM public.movies WHERE id = $1;';
-        try {
-          await pool.query(query, [id]);
-          return true;
-        } catch (error) {
-          throw error;
-        }
-      }
+  static async getAllMovies() {
+    const query = 'SELECT * FROM public.movies;';
+    try {
+      const { rows } = await pool.query(query);
+      return rows;
+    } catch (error) {
+      throw error;
     }
+  }
+
+  static addPost(objmovies, callback) {
+    const query = `INSERT INTO public.movies ("id", "title", "genres", "year", "photos") VALUES ($1, $2, $3, $4, $5);`;
+
+    const arrData = [objmovies.id, objmovies.title, objmovies.genres, objmovies.year, objmovies.photos];
+
+    pool.query(query, arrData, (err, result) => {
+      if (err) {
+        callback(err, null);
+      } else {
+        console.log(`${objmovies.title} sudah masuk datanya`);
+        callback(null, null);
+      }
+    });
+  }
+
+  static async updateMovie(id, updatedMovie) {
+    const { title, genres, year, photos } = updatedMovie;
+    const query = 'UPDATE public.movies SET title = $1, genres = $2, year = $3, photos = $4 WHERE id = $5';
+    try {
+      const { rows } = await pool.query(query, [title, genres, year, photos, id]);
+      return rows;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async deleteMovie(id) {
+    const query = 'DELETE FROM public.movies WHERE id = $1;';
+    try {
+      await pool.query(query, [id]);
+      return true;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
 
 module.exports = MoviesModel;
